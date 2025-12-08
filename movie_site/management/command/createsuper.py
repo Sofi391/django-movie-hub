b801a1bf8811cb28aea6e759e95288098b3289fd
Sofoniyas <sofi123man@gmail.com>
2025-12-08 19:09:22 +0300

    Add custom create superuser command and favicon

--- movie_site/management/command/createsuper.py
+++ movie_site/management/command/createsuper.py
@@ -1,12 +1,17 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
-    help = 'Create superuser if not exists'
+    help = "Create a superuser if not exists"
 
     def handle(self, *args, **kwargs):
-        if not User.objects.filter(username='adminsofi').exists():
-            User.objects.create_superuser('adminsofi','sofiaaa3991@gmail.com','@sofidid123')
-            self.stdout.write('Superuser created')
+        User = get_user_model()
+        if not User.objects.filter(username="adminsofi").exists():
+            User.objects.create_superuser(
+                username="adminsofi",
+                email="sofiaaa3991@gmail.com",
+                password="@sofidid123"
+            )
+            self.stdout.write(self.style.SUCCESS("Superuser created!"))
         else:
-            self.stdout.write('Superuser already exists')
+            self.stdout.write(self.style.WARNING("Superuser already exists"))
